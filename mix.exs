fdf19f3cf8d3c5eca5cecc5da32cffc2b22130be
Wojnar <mateusz@sntx.pl>
2020-09-03 22:13:05 +0200

    Deps updated

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule EctoTranslate.Mixfile do
       app: :ecto_translate,
       description:
         "EctoTranslate is a library that helps with translating Ecto data. EctoTranslate can help you with returning translated values of your Ecto data attributes. For this it uses a singe table called 'translations' which will contain polymorphic entries for all of your Ecto data stucts.",
-      version: "1.0.2",
-      elixir: ">= 1.7.0",
+      version: "1.0.2-corus",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
